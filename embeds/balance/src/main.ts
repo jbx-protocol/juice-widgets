e02a8517c70cde1379696ac4ef459cfc28aa9776
aeolian <aeolian.eth@protonmail.com>
2022-05-13 01:06:22 +0930

    Error on project id missing

--- embeds/balance/src/main.ts
+++ embeds/balance/src/main.ts
@@ -17,8 +17,10 @@ const CONTRACTS_VERSION = "2";
 
 const formatWad = (wad: number) => (wad / 1e18).toFixed(2);
 const getOptions = () => {
+  console.log(window);
   const projectId = window.location.hash.match(V2_PATH_REGEX)?.[1];
-  if (!projectId) throw new Error();
+  if (!projectId)
+    throw new Error(`No project id [location=${window.location}]`);
 
   return {
     projectId,

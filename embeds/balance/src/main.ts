eb3b0c5161e47971f6eefc57dab3a29170133025
aeolian <aeolian.eth@protonmail.com>
2022-05-13 00:40:18 +0930

    Fetch total paid from subgraph

--- embeds/balance/src/main.ts
+++ embeds/balance/src/main.ts
@@ -1,8 +1,53 @@
-import './style.css'
+import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const V2_PATH_REGEX = /#\/v2\/p\/([0-9+])/;
+const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/id/QmRoxhw8zQzsVpVj8Mf4hs5bYwTxTxyvZEpHPLZ6shVsXy";
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const projectQuery = `query project($id: ID!) {
+  project(id: $id) {
+    totalPaid
+  }
+}
+`;
+const CONTRACTS_VERSION = "2";
+
+const formatWad = (wad: number) => (wad / 1e18).toFixed(2);
+
+const getOptions = () => {
+  const projectId = window.location.hash.match(V2_PATH_REGEX)?.[1];
+  if (!projectId) throw new Error();
+
+  return {
+    projectId,
+  };
+};
+
+const fetchProject = (id: string) => {
+  return fetch(SUBGRAPH_URL, {
+    method: "POST",
+    body: JSON.stringify({ query: projectQuery, variables: { id } }),
+  }).then((response) => response.json());
+};
+
+const renderApp = async () => {
+  const app = document.querySelector<HTMLDivElement>("#app")!;
+
+  try {
+    const options = getOptions();
+    const id = `${CONTRACTS_VERSION}-${options.projectId}`;
+
+    const res = await fetchProject(id);
+    if (!res.data?.project?.totalPaid) {
+      throw new Error("Failed to fetch project.");
+    }
+
+    const paid = formatWad(parseInt(res.data?.project?.totalPaid));
+
+    app.innerHTML = `${paid} ETH raised`;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+renderApp();

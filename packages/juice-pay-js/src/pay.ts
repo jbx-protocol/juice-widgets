c0831ba1d201d6ac3a5390bab8c229ef96022bf1
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-09-05 11:52:22 +0930

    Nicer mount

--- packages/juice-pay-js/src/pay.ts
+++ packages/juice-pay-js/src/pay.ts
@@ -1,12 +1,19 @@
-const scriptEl = document.querySelector("script"); // TODO scope to the correct src attr
+// const SRC_URL = "../../../public/dist/pay.js";
+const SRC_URL = "https://tools.juicebox.money/pay.js";
+
+const scriptEl = document.querySelector(`script[src="${SRC_URL}"]`) as
+  | HTMLScriptElement
+  | undefined;
 const options = Object.assign({}, scriptEl?.dataset);
+
 const IFRAME_STYLE_ACTIVE =
   "z-index: 2147483647; background: rgba(0, 0, 0, 0.004); border: 0px none transparent; overflow: hidden auto; visibility: visible; margin: 0px; padding: 0px; -webkit-tap-highlight-color: transparent; position: fixed; left: 0px; top: 0px; width: 100%; height: 100%;";
 const IFRAME_STYLE_INACTIVE =
   "z-index: 2147483647; display: none; background: rgba(0, 0, 0, 0.004); border: 0px none transparent; overflow: hidden auto; visibility: visible; margin: 0px; padding: 0px; -webkit-tap-highlight-color: transparent; position: fixed; left: 0px; top: 0px; width: 100%; height: 100%;";
 
 function createIframe() {
-  const iframeSrc = "https://juice-tools-embed-pay.netlify.app/";
+  // const iframeSrc = "https://juice-tools-embed-pay.netlify.app/";
+  const iframeSrc = "http://localhost:3000";
 
   const iframe = document.createElement("iframe");
 
@@ -44,8 +51,8 @@ function mount() {
 
   addFrameEventListeners(iframe);
 
-  scriptEl?.insertAdjacentElement("afterend", iframe);
-  scriptEl?.insertAdjacentElement("beforebegin", button);
+  const mountEl = document.getElementById("juice-pay-button");
+  mountEl?.replaceWith(iframe, button);
 }
 
 mount();

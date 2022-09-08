36d8b8b47b4f19314036f88abcfcb3bf81050498
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2022-09-08 19:58:14 +0930

    rm border

--- embed/project/pay/src/components/Logo.tsx
+++ embed/project/pay/src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export function Logo({ src }: { src: string }) {
   return (
     <img
-      className="inline-block h-14 w-14 -mt-7 border-white border"
+      className="inline-block h-14 w-14 -mt-7"
       src={src}
       alt="Juicebox project logo"
       width="56"

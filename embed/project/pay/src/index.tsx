2776d3d8cb0dcec02427fa7b341e0bf773c19870
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-09-10 08:04:26 +0200

    remove juice-provider

--- embed/project/pay/src/index.tsx
+++ embed/project/pay/src/index.tsx
@@ -1,39 +1,20 @@
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { JuiceProvider as DefaultJuice } from "juice-hooks";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { useProvider, useSigner, WagmiConfig } from "wagmi";
+import { WagmiConfig } from "wagmi";
 import App from "./App";
 import { AppContext, AppOptions } from "./contexts/AppContext";
 import "./index.css";
 import { createDefaultClient } from "./lib/wagmi/createClient";
-import { NetworkName } from "./types";
-
-const JuiceProvider = ({
-  networkName,
-  children,
-}: {
-  networkName?: NetworkName;
-  children: JSX.Element;
-}) => {
-  const provider = useProvider();
-  const { data: signer } = useSigner();
-
-  return (
-    <DefaultJuice provider={signer ?? provider} networkName={networkName}>
-      {children}
-    </DefaultJuice>
-  );
-};
 
 const renderApp = (options: AppOptions) => {
   const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
   );
 
-  console.log("juice pay opts", options);
+  console.log("🧃 juice pay opts", options);
 
   if (options.version !== "latest") {
     throw new Error("unsupported project version.");
@@ -47,11 +28,9 @@ const renderApp = (options: AppOptions) => {
       <WagmiConfig client={wagmiClient}>
         <RainbowKitProvider chains={chains}>
           <AppContext.Provider value={{ options, root }}>
-            <JuiceProvider networkName={options.networkName ?? "mainnet"}>
-              <QueryClientProvider client={queryClient}>
-                <App />
-              </QueryClientProvider>
-            </JuiceProvider>
+            <QueryClientProvider client={queryClient}>
+              <App />
+            </QueryClientProvider>
           </AppContext.Provider>
         </RainbowKitProvider>
       </WagmiConfig>

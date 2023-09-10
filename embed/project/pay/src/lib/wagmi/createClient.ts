d133478279a81dfb77c30fdfd0d9851da4bd6546
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-09-10 08:07:50 +0200

    support goerli proper

--- embed/project/pay/src/lib/wagmi/createClient.ts
+++ embed/project/pay/src/lib/wagmi/createClient.ts
@@ -2,12 +2,13 @@ import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient } from "wagmi";
 import { infuraProvider } from "wagmi/providers/infura";
 import { publicProvider } from "wagmi/providers/public";
+import { NetworkName } from "../../types";
 
 const infuraKey = "c2838024e339438fbe8a31d6754efe8a";
 
-export const createDefaultClient = () => {
+export const createDefaultClient = (network: NetworkName) => {
   const { chains, provider } = configureChains(
-    [chain.mainnet, chain.rinkeby],
+    [network === "goerli" ? chain.goerli : chain.mainnet],
     [infuraProvider({ apiKey: infuraKey }), publicProvider()]
   );
 
